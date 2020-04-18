f98c9588d86c04da3d51720d6b48a32f8c22283b
fbronchaly <fbronchaly@gmail.com>
2020-04-18 12:57:04 +0200

    2 files modified, 12 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { ArticleComponent } from './article/article.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { navComponent } from './nav/nav.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, ArticleComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
